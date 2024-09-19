483d5c2f358ad76d9a247506886bd3dede933e01
303 <303@303NEW-15>
2024-09-19 16:20:07 +0300

    Добавьте файлы проекта.

---
+++ LR3/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LR3.UsersClasses;
+using static LR3.UsersClasses.InfoEmailSending;
+
+namespace LR3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "task_code_development@list.ru";
+            textBoxName.Text = "Антон Паокин";
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            //проверка TextBox на наличие значений
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text) ||
+            string.IsNullOrWhiteSpace(textBoxName.Text) ||
+            string.IsNullOrWhiteSpace(textBoxSubject.Text) ||
+            string.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            // Ввод данных с формы в объекты ранее созданных классов 
+            string smtp = "smtp.mail.ru";
+            // Необходимо ввести свой mail.ru адрес!!! И своё ФИО 
+            StringPair fromInfo = new StringPair("katenasovamail364@mail.ru", "Катя Катерина");
+            // Необходимо ввести свой пароль который вывел mail.ru !!!
+            string password = "pvEpDMiNbLwfwa9Z7heP";
+
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text;
+            string body = $"{DateTime.Now} \n" +
+            $"{Dns.GetHostName()} \n" +
+            $"{Dns.GetHostAddresses(Dns.GetHostName()).First()} \n" +
+            $"{textBoxBody.Text}";
+
+            InfoEmailSending info =
+            new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+            //Отправка данных в виде электронного письма
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+
+            //Уведомления для пользователя и очистка всех TextBox 
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LR3/UsersClasses/SendingEmail.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace LR3.UsersClasses
+{
+    public class SendingEmail
+    {
+        private InfoEmailSending InfoEmailSending { get; set; }
+
+        public SendingEmail(InfoEmailSending infoEmailSending)
+        {
+            InfoEmailSending = infoEmailSending
+            ?? throw new ArgumentNullException(nameof(infoEmailSending));
+        }
+
+        public void Send()
+        {
+            // Добавляем обработку исключений
+            try
+            {
+                // Вносим адрес ЅМТР сервера
+                SmtpClient mySmtpClient =
+                new SmtpClient(InfoEmailSending.SmtpClientAdress);
+                // Задаем учётные данные пользователя 
+                mySmtpClient.UseDefaultCredentials = false;
+                // Включаем ипользование портокола SSl
+                mySmtpClient.EnableSsl = true;
+                // Задаём учётные данные пользователя 
+                NetworkCredential basicAuthenticationInfo = new
+                NetworkCredential(
+                InfoEmailSending.EmailAdressFrom.EmailAdress,
+                InfoEmailSending.EmailPassword);
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+                // Добавляем адрес откуда отправлнено сообщение 
+                MailAddress from = new MailAddress(
+                InfoEmailSending.EmailAdressFrom.EmailAdress,
+                InfoEmailSending.EmailAdressFrom.Name);
+                // Добавляем адрес куда будет отправлнено сообщение
+                MailAddress to = new MailAddress(
+                InfoEmailSending.EmailAdressTo.EmailAdress,
+                InfoEmailSending.EmailAdressTo.Name);
+                MailMessage myMail = new MailMessage(from, to);
+                // Добавляем наш адрес в список адресов для ответа
+                MailAddress replyTo =
+                new MailAddress(InfoEmailSending.EmailAdressFrom.EmailAdress);
+                myMail.ReplyToList.Add(replyTo);
+                // Выбираем кодировку символов в письме
+                //В нашем случае UTF8
+                Encoding encoding = Encoding.UTF8;
+                // Задаем значение Заголовка и его кодировку
+                myMail.Subject = InfoEmailSending.Subject;
+                myMail.SubjectEncoding = encoding;
+                // Задаем значение Сообщения и его кодировку 
+                myMail.Body = InfoEmailSending.Body;
+                myMail.BodyEncoding = encoding;
+                // Отправляем письмо
+                mySmtpClient.Send(myMail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
